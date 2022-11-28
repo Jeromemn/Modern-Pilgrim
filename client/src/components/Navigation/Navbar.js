1173a33546bb8cf674a7f19998368d8a6ae3fc39
nixon <jeromenixon95@gmail.com>
2022-11-28 11:22:47 -0700

    saving to pull teams work

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -1,18 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function NavTabs() {
   return (
     <>
       <ul>
-        <li href='#My-profile' onClick={() => handlePageChange('MyProfile')}>My Profile</li>
+        <li>
+          <Link to="/myProfile/"> ${Auth.getProfile().data.username} </Link>
+          My Profile
+        </li>
 
-        <li href='#Submit' onClick={() => handlePageChange('MyProfile')}>Submit a Voyage</li>
+        {/* // href='#My-profile' onClick={() => handlePageChange('MyProfile')} */}
 
-        <li href='#Explore' onClick={() => handlePageChange('Explore')} >Explore</li>
+        <li href="#Submit" onClick={() => handlePageChange("MyProfile")}>
+          Submit a Voyage
+        </li>
 
-        <li href='#Login' onClick={() => handlePageChange('Login')}>Sign in</li>
+        <li href="#Explore" onClick={() => handlePageChange("Explore")}>
+          Explore
+        </li>
 
-        <li href='#Join' onClick={() => handlePageChange('Join')}>Sign up</li>
+        <li href="#Login" onClick={() => handlePageChange("Login")}>
+          Sign in
+        </li>
+
+        <li href="#Join" onClick={() => handlePageChange("Join")}>
+          Sign up
+        </li>
       </ul>
     </>
   );
