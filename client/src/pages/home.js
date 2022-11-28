96ff12042ed7f64ae410872e740c5dceaa18a876
Joy Halliday <tjoyhalliday@gmail.com>
2022-11-28 13:49:22 -0700

    created file structure for pages and utils, created basic framework for mutations and queries

---
+++ client/src/pages/home.js
@@ -0,0 +1,1 @@
+//main exploring page
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,61 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+mutation login($username: String!, $password: String!) {
+login(username: $username, $password: $$password) {
+    token
+    user {
+        _id
+        username
+    }
+}
+}`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+        _id
+        username
+    }
+    }
+}
+`;
+
+export const ADD_TRIP = gql`
+mutation addTrip($tripText: String!) {
+    addTrip(tripText: $tripText) {
+    _id
+    tripText
+    tripLocation
+    tripPrice
+    tripRating
+    tripAuthor
+    createdAt
+    comments {
+        _id
+        commentText
+    }
+    }
+}
+`;
+
+export const ADD_COMMENT = gql`
+mutation addComment($tripId: ID!, $commentText: String!) {
+    addComment(tripId: $tripId, commentText: $commentText) {
+    _id
+    tripText
+    tripLocation
+    tripPrice
+    tripRating
+    tripAuthor
+    createdAt
+    comments {
+        _id
+        commentText
+        createdAt
+    }
+    }
+}
+`;
