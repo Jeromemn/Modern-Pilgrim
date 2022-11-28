4c74e09bc27a40c369d3a167ad5bc42b4e9b8d2d
Joy Halliday <tjoyhalliday@gmail.com>
2022-11-28 14:32:09 -0700

    add some auth0 boilerplate, in progress

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
+import ReactDOM from "react-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="YOUR_DOMAIN"
+    clientId="YOUR_CLIENT_ID"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
