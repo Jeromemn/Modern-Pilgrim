5ff140768d37be8abb45c468ca855e061238919c
Joy Halliday <tjoyhalliday@gmail.com>
2022-11-29 14:43:45 -0700

    updating profiles page in progress

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Auth0Provider } from "@auth0/auth0-react";
-=======
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -14,7 +6,6 @@ import App from './App';
 import { Auth0Provider } from "@auth0/auth0-react";
 
 
->>>>>>> ba615db79ea314b6874a497b01514a417d67bb47
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
